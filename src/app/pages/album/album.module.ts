20386b2c3e5dace9c36f0d97bb48d6198f1e5ed1
Ronny Arruda <ronnyapinho@gmail.com>
2021-01-11 14:34:56 -0400

    inclusão de modulo de album

--- src/app/pages/album/album.module.ts
+++ src/app/pages/album/album.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AlbumComponent } from './album.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AlbumComponent],
   imports: [
     CommonModule
   ]
