00263bf5cced5920a314af5dc41b0bd041173874
Ronny Arruda <ronnyapinho@gmail.com>
2021-01-14 16:22:46 -0400

    paginação no front end

--- src/app/pages/album/album-list/album-list.component.ts
+++ src/app/pages/album/album-list/album-list.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
 import { ActivatedRoute } from '@angular/router';
 import { NewsPost, NewsService } from 'app/pages/layout/news.service';
 import { environment } from 'environments/environment';
@@ -12,66 +13,39 @@ import { AlbumService } from '../album.service';
 export class AlbumListComponent implements OnInit {
 
   private baseUrl: string;
-  pageSize = 10
-  news = new Array<NewsPost>();
-  items = Array.from({length: 100000}).map((_, i) => `Item #${i}`);
-  firstCard = {
-    news: [],
-    placeholders: [],
-    loading: false,
-    pageToLoadNext: 1,
-  };
+  length = 0;
+  pageSize = 5;
+  pageNumber = 0;
+  searchResults: Array<any>;
+  pageEvent: PageEvent;
 
   constructor(
     private service: AlbumService,
-    private newsService: NewsService,
     public activatedRoute: ActivatedRoute,
   ) {
     this.activatedRoute.data.subscribe(data => {
       this.baseUrl = environment.api + data.endPoint
     });
-
   }
 
   ngOnInit(): void {
-    this.loadNext(this.firstCard);
-    // this.service.getAllPaginate(0, 1, this.baseUrl, null)
-    //   .subscribe(res => {
-    //     console.log(res.contents)
-    //   });
+    this.loadALbum(this.pageNumber, this.pageSize);
   }
 
-  // loadNext(cardData) {
-  //   if (cardData.loading) { return; }
-
-  //   cardData.loading = true;
-  //   cardData.placeholders = new Array(this.pageSize);
-  //   this.service.getAllPaginate(cardData.pageToLoadNext, this.pageSize, this.baseUrl, null)
-  //     .subscribe(res => {
-  //       if(res.contents.length > 0){
-  //         cardData.news.push(res.contents);
-  //         cardData.pageToLoadNext++;
-  //         cardData.loading = false;
-  //         cardData.placeholders = [];
-  //       } 
-  //     });
-  // }
-
-  loadNext(cardData) {
-    console.log(cardData)
-    if (cardData.loading) { return; }
-
-    cardData.loading = true;
-    cardData.placeholders = new Array(this.pageSize);
-    this.newsService.load(0,50)
-      .subscribe(nextNews => {
-        this.news.push(...nextNews);
-        cardData.placeholders = [];
-        cardData.news.push(...nextNews);
-        cardData.loading = false;
-        cardData.pageToLoadNext++;
+  private loadALbum(pageNumber: number, pageSize: number) {
+    this.service.getAllPaginate(pageNumber, pageSize, this.baseUrl, null)
+      .subscribe(res => {
+        if (res.contents.length > 0) {
+          this.searchResults = res.contents;
+          this.length = res.pageInfo.totalElements;
+        }
       });
   }
 
+  setPageEvent(pageEvent: PageEvent) {
+    this.loadALbum(pageEvent.pageIndex, pageEvent.pageSize)
+    console.log(pageEvent)
+  }
 
 }
+
--- src/app/pages/album/album.module.ts
+++ src/app/pages/album/album.module.ts
@@ -1,12 +1,13 @@
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { NbButtonModule, NbCardModule, NbLayoutModule } from '@nebular/theme';
 import { ThemeModule } from 'app/@theme/theme.module';
-import { NewsService } from '../layout/news.service';
 import { AlbumListComponent } from './album-list/album-list.component';
 import { NewsPostPlaceholderComponent } from './album-list/news-post-placeholder/news-post-placeholder.component';
 import { AlbumRoutingModule } from './album-routing.module';
+import { AlbumService } from './album.service';
 
 @NgModule({
   declarations: [AlbumListComponent, NewsPostPlaceholderComponent],
@@ -18,9 +19,10 @@ import { AlbumRoutingModule } from './album-routing.module';
     NbLayoutModule,
     NbButtonModule,
     ScrollingModule,
+    MatPaginatorModule,
   ],
   providers: [
-    NewsService,
+    AlbumService,
   ],
 })
 export class AlbumModule { }
